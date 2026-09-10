cb987c02aafdf2d605451732e6261843ba99d6df
valer <lirskikh@mail.ru>
2026-09-10 15:18:48 +0500

    Task1

---
+++ Tyuiu.ByachkovaVD.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.ByachkovaVD.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.ByachkovaVD.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Валерия"));
+            Console.ReadKey();
+       
+        }
+    }
+}
